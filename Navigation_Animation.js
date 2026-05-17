96070e2320470e103a30447b0415cb154f3b16f1
Lappy_Decadenza <jackieson235@mgail.com>
2026-05-17 21:06:01 +0700

    No change

--- Navigation_Animation.js
+++ Navigation_Animation.js
@@ -1,11 +1,11 @@
-// Hàm xử lý trượt thanh truyện nổi bật
+//Hàm xử lý trượt thanh truyện nổi bật
 function slideCarousel(direction) {
   const container = document.getElementById("story-list-container");
 
-  // Đo chiều rộng hiển thị hiện tại của khung truyện
+  //Đo chiều rộng hiển thị hiện tại của khung truyện
   const scrollAmount = container.clientWidth;
 
-  // Trượt qua trái (-1) hoặc phải (1) với khoảng cách bằng đúng 1 khung hiển thị
+  //Trượt qua trái (-1) hoặc phải (1) với khoảng cách bằng đúng 1 khung hiển thị
   container.scrollBy({
     left: direction * scrollAmount,
     behavior: "smooth",
